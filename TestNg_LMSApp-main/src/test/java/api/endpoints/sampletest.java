2d305c567281b97e40578fffd4413c8d621853f5
monika <marihalm47@gmail.com>
2023-08-23 19:06:57 -0500

    This is cloned modified changes code...

---
+++ TestNg_LMSApp-main/src/test/java/api/endpoints/sampletest.java
@@ -0,0 +1,10 @@
+package api.endpoints;
+
+public class sampletest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Monika updated code tested....");
+	}
+
+}
